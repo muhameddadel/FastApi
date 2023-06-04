1e0f4324d80f3acafd5488e15f1886b3f59ba5e1
muhameddadel <muhameddadel2000@gmail.com>
2023-06-04 19:27:38 +0300

    get and post

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -16,6 +17,11 @@ class Post(BaseModel):
 my_posts = [{"title": "Hello title", "content": "Hello content", "id": 1}, {"title": "Hello titlee", "content": "Hello contentt", "id": 2}]
 
 
+def find_post(id):
+    for post in my_posts:
+        if post['id'] == id:
+            return post
+
 @app.get("/")
 @app.get("/login")
 def root():
@@ -27,10 +33,19 @@ def get_posts():
     return {'data': my_posts}
 
 
-@app.post('/posts')
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    print(post.title)
-    print(post.content)
-    print(post.boolean)
-    print(post.rating)
-    return {'post': post}
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000)
+    my_posts.append(post_dict)
+    return {'post': post_dict}
+
+
+@app.get('/posts/{id}')
+def get_post(id: int, response: Response):
+    
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{id} do not found")
+
+    return {'post_detail': post}

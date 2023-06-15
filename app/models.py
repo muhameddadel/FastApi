b494cba53f36fb5cb407211f4793d628c3c87f1b
muhameddadel <muhameddadel2000@gmail.com>
2023-06-15 23:08:00 +0300

    relatioship between User and Post tables

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
@@ -12,6 +12,8 @@ class Posts(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+
 
 class User(Base):
     __tablename__ = 'users'
--- app/schema.py
+++ app/schema.py
@@ -13,6 +13,10 @@ class PostCreate(PostBase):
 
 
 class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+    user_id: int
+    
     class Config:
         orm_mode = True
 

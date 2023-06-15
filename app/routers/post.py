ee8e6a5c4cadf64621cba28e5cc512214fe94848
muhameddadel <muhameddadel2000@gmail.com>
2023-06-15 23:20:01 +0300

    fixing sqlalchemy issue

--- app/routers/post.py
+++ app/routers/post.py
@@ -27,7 +27,7 @@ def create_post(post: PostCreate, db: Session = Depends(get_db), current_user: i
     #                 (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
-    new_post = models.Posts(**post.dict())
+    new_post = models.Posts(user_id=current_user.id,**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)

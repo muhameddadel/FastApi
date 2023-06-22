64eb68708e9e9edafd38b54e4df5093c7aace7f7
muhameddadel <muhameddadel2000@gmail.com>
2023-06-22 23:26:56 +0300

    parametrize

--- app/tests/test_calc.py
+++ app/tests/test_calc.py
@@ -1,5 +1,19 @@
-from app.calculations import add
+import pytest
+from app.calculations import *
 
-def test_add():
-    
-    assert add(5, 3) == 8
+
+@pytest.mark.parametrize("num1, num2, expected", [(2, 3, 5), (5, 5, 10)])
+def test_add(num1, num2, expected):
+    assert add(num1, num2) == expected
+
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+
+def test_myltiply():    
+    assert multiply(5, 3) == 15
+
+
+def test_divde():
+    assert divide(6, 3) == 2

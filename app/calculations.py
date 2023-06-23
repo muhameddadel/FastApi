f23f7d4f836705a4c1c4aa9b8da7b4554682b5f1
muhameddadel <muhameddadel2000@gmail.com>
2023-06-23 18:45:26 +0300

    create test for root

--- app/calculations.py
+++ app/calculations.py
@@ -1,14 +1,30 @@
-def add(num1: int, num2: int):
-    return num1 + num2
+# def add(num1: int, num2: int):
+#     return num1 + num2
 
 
-def subtract(num1: int, num2: int):
-    return num1 - num2
+# def subtract(num1: int, num2: int):
+#     return num1 - num2
 
 
-def multiply(num1: int, num2: int):
-    return num1 * num2
+# def multiply(num1: int, num2: int):
+#     return num1 * num2
 
 
-def divide(num1: int, num2: int):
-    return num1 / num2
+# def divide(num1: int, num2: int):
+#     return num1 / num2
+
+
+# class BankAccount():
+#     def __init__(self, starting_balance=0) -> None:
+#         self.balance = starting_balance
+
+#     def deposit(self, amount):
+#         self.balance += amount
+
+#     def withdraw(self, amount):
+#         if amount > self.balance:
+#             raise Exception("Your balance is less than the amount")
+#         self.balance -= amount
+
+#     def collect_interest(self, amount):
+#         self.balance *= 1.1
--- app/tests/test_calc.py
+++ app/tests/test_calc.py
@@ -1,19 +1,37 @@
-import pytest
-from app.calculations import *
+# import pytest
+# from app.calculations import *
 
 
-@pytest.mark.parametrize("num1, num2, expected", [(2, 3, 5), (5, 5, 10)])
-def test_add(num1, num2, expected):
-    assert add(num1, num2) == expected
+# @pytest.fixture
+# def zero_bank_account():
+#     return BankAccount()
 
 
-def test_subtract():
-    assert subtract(5, 3) == 2
+# @pytest.fixture
+# def bank_account():
+#     return BankAccount(50)
 
 
-def test_myltiply():    
-    assert multiply(5, 3) == 15
+# @pytest.mark.parametrize("num1, num2, expected", [(2, 3, 5), (5, 5, 10)])
+# def test_add(num1, num2, expected):
+#     assert add(num1, num2) == expected
 
 
-def test_divde():
-    assert divide(6, 3) == 2
+# def test_subtract():
+#     assert subtract(5, 3) == 2
+
+
+# def test_myltiply():    
+#     assert multiply(5, 3) == 15
+
+
+# def test_divde():
+#     assert divide(6, 3) == 2
+
+
+# def test_bank_initial_amount(bank_account):
+#     assert bank_account.balance == 50
+
+# def test_insufficient_funds(bank_account):
+#     with pytest.raises(Exception):
+#         bank_account.withdraw(200)

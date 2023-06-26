913bf8055b926ee4c0d3492782e16eda6b2c3c4f
muhameddadel <muhameddadel2000@gmail.com>
2023-06-26 19:13:53 +0300

    add database fixture

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -1,43 +1,5 @@
-from fastapi.testclient import TestClient
-import pytest
-from app.main import app
 from app import schema
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.config import settings
-from app.database import get_db
-from app.database import Base
-
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        
-app.dependency_overrides[get_db] = override_get_db
-
-
-@pytest.fixture
-def client():
-    # sqlalchemy
-    Base.metadata.create_all(bind=engine)
-    Base.metadata.drop_all(bind=engine)
-    # alembic
-    # comand.upgrade("head")
-    # comand.downgrade("head")
-    yield TestClient(app)
+from .test_database import client, session
 
 def test_root(client):
     response = client.get('/')

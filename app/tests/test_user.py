b05fe4256a1b85dac101c49e1c50d623d12c71dc
muhameddadel <muhameddadel2000@gmail.com>
2023-06-28 19:40:40 +0300

    login user validation test

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -1,10 +1,19 @@
+import pytest
+from jose import jwt
 from app import schema
 from .test_database import client, session
+from app.config import settings
 
-def test_root(client):
-    response = client.get('/')
-    assert response.json().get("message") == "Hello"
-    assert response.status_code == 200
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "medo@gmail.com", "password": "password123"}
+    response = client.post("/users/", json=user_data)
+
+    assert response.status_code == 201
+
+    new_user = response.json()
+    new_user['password'] = user_data['password']
+    return new_user
 
 def test_create_user(client):
     response = client.post("/users/", json={"email": "madel@gmail.com", "password": "password123"})
@@ -14,7 +23,11 @@ def test_create_user(client):
     assert response.status_code == 201
 
 
-def test_login_user(client):
-    response = client.post("/login", data={"username": "madel@gmail.com", "password": "password123"})
-
+def test_login_user(client, test_user):
+    response = client.post("/login", data={"username": test_user["email"], "password": test_user["password"]})
+    login_response = schema.Token(**response.json())
+    payload = jwt.decode(login_response.access_token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_response.token_type == 'bearer'
     assert response.status_code == 200
